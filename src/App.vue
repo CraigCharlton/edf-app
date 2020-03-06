e41e3d17c74d5aef2980e785d6bf50486bca1a8d
CraigCharlton <craig.charlton@bedegaming.com>
2020-03-06 11:06:56 +0000

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="EDF Logo" class="main-logo" src="./assets/EDFLogo.png">
+    <Home title="Home"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
@@ -24,5 +24,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.main-logo {
+  max-width: 250px;
 }
 </style>
