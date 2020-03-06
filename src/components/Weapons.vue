13b6fedd72b96aeeffd3934c7db08cdeda2acc61
CraigCharlton <craig.charlton@bedegaming.com>
2020-03-06 15:30:57 +0000

    Weapon component base html added

---
+++ src/components/Weapons.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="weapons">
+    <div class="input-container name">
+      <label>Weapon name:</label>
+      <input />
+    </div>
+    <div class="input-container level">
+      <label>Level:</label>
+      <input />
+    </div>
+    <div class="input-container min">
+      <label>Min weapon lvl:</label>
+      <input />
+    </div>
+    <div class="input-container max">
+      <label>Max weapon lvl:</label>
+      <input />
+    </div>
+    <div class="submit">
+      <button>Submit</button>
+    </div>
+    <div class="results">
+      <input type="text" value="Results" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Weapons',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+  .weapons {
+    margin-top: 10px;
+    margin: 30px auto 0;
+    max-width: 350px;
+    text-align: right;
+  }
+
+  .input-container {
+    margin-bottom: 10px;
+  }
+
+  .results input {
+    height: 80px;
+  }
+</style>
--- src/views/EDF4-1.vue
+++ src/views/EDF4-1.vue
@@ -3,12 +3,18 @@
     <img alt="EDF Logo" class="main-logo" src="../assets/EDF41Logo.png" />
     <h1>EDF 4.1</h1>
     <p>EDF 4.1 lists</p>
+    <Weapons />
   </div>
 </template>
 
 <script>
+import Weapons from '../components/Weapons.vue'
+
 export default {
-  name: "EDF4-1"
+  name: "EDF4-1",
+  components: {
+    Weapons
+  }
 };
 </script>
 
